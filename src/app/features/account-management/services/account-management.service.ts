583f7a4756a0a4f61a09044ec95689c5fe3d3651
FrancisFrancois <francisfrancois9189@gmai.com>
2023-11-15 11:52:49 +0100

    create-account.ts, account-managment.service, registerAccount.ts

--- src/app/features/account-management/services/account-management.service.ts
+++ src/app/features/account-management/services/account-management.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountManagementService {
 
-  constructor() { }
+  private _url: string = 'http://localhost:3000/api/account-management';
+
+  constructor(private _httpClient: HttpClient) { }
+  
 }

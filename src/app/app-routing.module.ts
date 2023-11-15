cf2fb5e5fbdc85b23e93bdcdf7e592eacab4e73e
FrancisFrancois <francisfrancois9189@gmai.com>
2023-11-15 11:20:47 +0100

    app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAccountComponent } from './features/account-management/components/create-account/create-account.component';
+import { ReadAccountComponent } from './features/account-management/components/read-account/read-account.component';
+import { UpdateAccountComponent } from './features/account-management/components/update-account/update-account.component';
 
 const routes: Routes = [
   { path : "create-account", component: CreateAccountComponent },
+  { path : "read-account", component: ReadAccountComponent },
+  { path : "update-account", component: UpdateAccountComponent },
 ];
 
 @NgModule({

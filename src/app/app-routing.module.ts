531fb7e8cc0023a186ac0a5bc8f9af01781af6fe
RobinPecheurTechnobel <robin.pecheur@edu.technobel.be>
2023-11-28 11:33:24 +0100

    refactor route
    ajout de l'id dans les route update et create des users et owner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,12 +14,12 @@ import { AuthComponent } from './features/auth/components/auth.component';
 const routes: Routes = [
   { path : "create-account", component: CreateAccountComponent },
   { path : "list-account", component: ListAccountComponent },
-  { path : "read-account", component: ReadAccountComponent },
-  { path : "update-account", component: UpdateAccountComponent },
+  { path : "read-account/:id", component: ReadAccountComponent },
+  { path : "update-account/:id", component: UpdateAccountComponent },
   { path : "owner-management", component: OwnerManagementComponent },
-  { path : "read-owner", component: ReadOwnerComponent },
+  { path : "read-owner/:id", component: ReadOwnerComponent },
   { path : "list-owner", component: ListOwnerComponent },
-  { path : "update-owner", component: UpdateOwnerComponent },
+  { path : "update-owner/:id", component: UpdateOwnerComponent },
   { path : "not-found", component: NotfoundComponent },
   { path : "auth", component : AuthComponent}
 ];

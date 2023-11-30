64f1ef1fcdbca7e9b75b2c33f58c39833ff6e34b
FrancisFrancois <francisfrancois9189@gmai.com>
2023-11-30 16:59:55 +0100

    modification css owner-managment pour figma

--- src/app/features/account-management/components/update-account/update-account.component.ts
+++ src/app/features/account-management/components/update-account/update-account.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { AccountManagementService } from '../../../../shared/services/account-management.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UpdateAccount } from '../../models/registerAccount';
 
 @Component({
   selector: 'app-update-account',

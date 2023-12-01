4e4866b7eb172bcd4dc86a745373757978314de6
François Francis <francisfrancois9189@gmail.com>
2023-12-01 14:29:14 +0100

    modification html css ts create-account

--- src/app/features/account-management/components/create-account/create-account.component.ts
+++ src/app/features/account-management/components/create-account/create-account.component.ts
@@ -32,7 +32,7 @@ export class CreateAccountComponent {
 
   passwordMatchValidator(group: FormGroup): ValidationErrors | null {
     const passwordControl = group.get('password');
-    const confirmPasswordControl = group.get('passwordVerifiedd');
+    const confirmPasswordControl = group.get('passwordVerified');
 
     if (!passwordControl || !confirmPasswordControl) {
       return null; 

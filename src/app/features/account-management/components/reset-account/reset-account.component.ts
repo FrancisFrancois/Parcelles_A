0637d38c1ce09e4874b1d67ca4f1f1d7d35d3b63
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-05 15:38:41 +0100

    resetPassword reset=account.html et ts

--- src/app/features/account-management/components/reset-account/reset-account.component.ts
+++ src/app/features/account-management/components/reset-account/reset-account.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/features/auth/services/auth.service';
+import { AccountManagementService } from 'src/app/shared/services/account-management.service';
 
 @Component({
   selector: 'app-reset-account',
@@ -25,19 +25,58 @@ export class ResetAccountComponent {
     * constructeur du composant
     * 
     * @param _fb formbuilder créant une liaison avec le forumaire html
-    * @param _authService injection de dépendance du service d'authentification
+    * @param _accountManagementService injection de dépendance du service account management
     * @param _router injection de dépendance du service permettant de les redirection
     */
    constructor(
      private _fb : FormBuilder,
-     private _authService : AuthService,
+     private _accountManagementService : AccountManagementService,
      private _router : Router
      ) {
  
      this.resetPasswordForm = this._fb.group({
       password: [null, [Validators.required, Validators.maxLength(150), Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)]],
       passwordVerified: [null, [Validators.required]],
+     }, {
+       validators: this.passwordMatchValidator
      });
    }
 
+   /**
+   * Méthode définissant le fonctionnement du Validator vérifiant que la confirmation de mot de passe à la même valeur que le mot de passe
+   * 
+   * @param group le formulaire contenant les champs concernés
+   * @returns Une ValidationErrors si la condition n'est respecté | null si tout se passe bien
+   */
+  passwordMatchValidator(group: FormGroup): ValidationErrors | null {
+    const passwordControl = group.get('password');
+    const confirmPasswordControl = group.get('passwordVerified');
+
+    if (!passwordControl || !confirmPasswordControl) {
+      return null; 
+    }
+    const password = passwordControl.value;
+    const confirmPassword = confirmPasswordControl.value;
+
+    return password === confirmPassword ? null : { 'passwordMismatch': true };
+  }
+
+  resetPassword() : void {
+    if(this.resetPasswordForm.valid){
+        this._accountManagementService.resetPassword(this.resetPasswordForm.value).subscribe({
+          next: (response) => {
+            console.log('Mot de passe reset avec succès', response);
+            this._router.navigateByUrl('/auth');
+          },
+          error: (error) => {
+            console.error('Erreur pendant le reset du mot de passe', error);
+          },
+          complete: () => {
+            console.log('Reset du mot de passe terminé');
+          }
+        });
+    } else {
+    this.resetPasswordForm.markAllAsTouched();
+  }
+}
 }

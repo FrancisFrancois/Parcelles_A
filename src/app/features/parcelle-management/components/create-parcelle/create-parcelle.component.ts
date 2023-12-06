6bf86f403cc08f0957ea720ed12404d35271eba8
Hélène Léonard <helene.leonard@outlook.fr>
2023-12-06 16:41:52 +0100

    en cours : affichage owners dans indivision

--- src/app/features/parcelle-management/components/create-parcelle/create-parcelle.component.ts
+++ src/app/features/parcelle-management/components/create-parcelle/create-parcelle.component.ts
@@ -62,7 +62,6 @@ export class CreateParcelleComponent {
   // filteredOwnersFname: any[] = [];
   constructor() { }
 
-  ngOnInit() { }
 
   
   // owners = [
@@ -78,64 +77,97 @@ export class CreateParcelleComponent {
   zip: string = "";
   email: string = "";
   selectedOwner: any;
-SearchOwner(): void {
-  clearTimeout(this.timeout);
-  this.timeout = null;
-
-  const total = this.fname + this.lname + this.zip + this.email;
-
-  this.timeout = setTimeout(() => {
-    this.owners = [
-      { fname: 'alain', lname: 'leonard', zip: '12345', email: 'alain@example.com' },
-      { fname: 'romain', lname: 'dupouy', zip: '67890', email: 'romain@example.com' },
-      { fname: 'louis', lname: 'dupont', zip: '54321', email: 'louis@example.com' },
-    ];
-
-    // Filtrer les propriétaires uniquement si lname n'est pas vide
-    if (this.lname && total) {
-      this.owners = this.owners.filter(owner =>
-        owner.lname.toLowerCase().startsWith(this.lname.toLowerCase()) &&
-        owner.fname.toLowerCase().includes(this.fname.toLowerCase()) &&
-        owner.zip.includes(this.zip) &&
-        owner.email.toLowerCase().includes(this.email.toLowerCase())
-      );
-    } else {
-      this.owners = []; // Si lname est vide, pas de résultats
-    }
-
-    console.log(this.owners);
-
+  SearchOwner(): void {
     clearTimeout(this.timeout);
     this.timeout = null;
-  }, 500);
-}
 
-// ...
+    const total = this.fname + this.lname + this.zip + this.email;
+
+    this.timeout = setTimeout(() => {
+      this.owners = [
+        { fname: 'alain', lname: 'leonard', zip: '12345', email: 'alain@example.com' },
+        { fname: 'romain', lname: 'dupouy', zip: '67890', email: 'romain@example.com' },
+        { fname: 'louis', lname: 'dupont', zip: '54321', email: 'louis@example.com' },
+      ];
+
+      // Filtrer les propriétaires uniquement si lname n'est pas vide
+      if (this.lname && total) {
+        this.owners = this.owners.filter(owner =>
+          owner.lname.toLowerCase().startsWith(this.lname.toLowerCase()) &&
+          owner.fname.toLowerCase().startsWith(this.fname.toLowerCase()) &&
+          owner.zip.startsWith(this.zip) &&
+          owner.email.toLowerCase().startsWith(this.email.toLowerCase())
+        );
+      } else {
+        this.owners = []; // Si lname est vide, pas de résultats
+      }
+    
+
+      console.log(this.owners);
+
+      clearTimeout(this.timeout);
+      this.timeout = null;
+    }, 500);
+  }
+
 
   selectOwner(owner: any): void {
     this.selectedOwner = owner;
-
-    // Mettez à jour les autres champs avec les informations de l'objet sélectionné
     this.fname = owner.fname;
     this.lname = owner.lname;
     this.zip = owner.zip;
     this.email = owner.email;
-  }
-}
-// ...
 
+    this.owners = []; // pour enlever les suggestions après avoir cliquer sur le owner choisi
+  }
+
+
+  // Partie indiivision pour ajouter un propriétaire à une parcelle
+  addOwnerToParcel(): void {
+    // Vérifiez si les champs requis sont remplis
+    if (this.fname && this.lname && this.zip && this.email) {
+      // Ajoutez le propriétaire aux propriétaires existants
+      const newOwner = {
+        fname: this.fname,
+        lname: this.lname,
+        zip: this.zip,
+        email: this.email
+      };
   
-//   SearchOwnerLname() {
-//     this.filteredOwnersLname = this.owners.filter(owner =>
-//       owner.lname.toLowerCase().includes(this.searchTermLname.toLowerCase())
-//     );
-//     console.log(this.filteredOwnersLname);
-//   }
+      this.owners.push(newOwner);
   
-//   SearchOwnerFname() {
-//     this.filteredOwnersFname = this.owners.filter(owner =>
-//       owner.fname.toLowerCase().includes(this.searchTermFname.toLowerCase())
-//     );
-//     console.log(this.filteredOwnersFname);
-//   }
-// }
+      // Réinitialisez les champs d'entrée
+      this.fname = "";
+      this.lname = "";
+      this.zip = "";
+      this.email = "";
+  
+      // Affichez le nouveau propriétaire dans la liste
+      console.log("Propriétaire ajouté à la parcelle :", newOwner);
+    } else {
+      // Gérez le cas où les champs requis ne sont pas remplis (vous pouvez afficher un message d'erreur, par exemple)
+      console.log("Veuillez remplir tous les champs requis.");
+    }
+  }
+  
+
+  removeOwnerFromParcel(): void {
+    // Ajoutez votre logique pour supprimer un propriétaire de la liste (si nécessaire)
+    console.log("Propriétaire supprimé de la parcelle");
+  }
+  
+
+    //   SearchOwnerLname() {
+    //     this.filteredOwnersLname = this.owners.filter(owner =>
+    //       owner.lname.toLowerCase().includes(this.searchTermLname.toLowerCase())
+    //     );
+    //     console.log(this.filteredOwnersLname);
+    //   }
+  
+    //   SearchOwnerFname() {
+    //     this.filteredOwnersFname = this.owners.filter(owner =>
+    //       owner.fname.toLowerCase().includes(this.searchTermFname.toLowerCase())
+    //     );
+    //     console.log(this.filteredOwnersFname);
+    //   }
+  }
